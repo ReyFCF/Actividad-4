c599648408aaf041066b78aa7946bbeed2932702
MacBook Air <macbookair@Rey-Fernando.local>
2018-09-20 19:19:27 -0500

    first commit

---
+++ app/src/main/java/com/example/macbookair/actividad4/api/BookClient.java
@@ -0,0 +1,39 @@
+package com.example.macbookair.actividad4.api;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/**
+ * Created by macbookair on 06/09/18.
+ */
+
+public class BookClient {
+    private static final String API_BASE_URL = "http://openlibrary.org/";
+    private AsyncHttpClient client;
+
+    public BookClient() {
+        this.client = new AsyncHttpClient();
+    }
+
+    private String getApiUrl(String relativeUrl) {
+        return API_BASE_URL + relativeUrl;
+    }
+
+    // Method for accessing the search API
+    public void getBooks(final String query, JsonHttpResponseHandler handler) {
+        try {
+            String url = getApiUrl("search.json?q=");
+            client.get(url + URLEncoder.encode(query, "utf-8"), handler);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getExtraBookDetails(String openLibraryId, JsonHttpResponseHandler handler) {
+        String url = getApiUrl("books/");
+        client.get(url + openLibraryId + ".json", handler);
+    }
+}
